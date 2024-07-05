83a86016c26688bf3a1be0557ec7809ac712eca6
NekokeCore <core2002@163.com>
2024-07-05 22:56:10 +0800

    Fetch: Change streamType to STREAM_SYSTEM to reduce latency.

--- app/src/main/java/com/example/myapplication/AudioRecorder.kt
+++ app/src/main/java/com/example/myapplication/AudioRecorder.kt
@@ -9,7 +9,6 @@ import android.media.MediaRecorder
 import kotlin.concurrent.thread
 
 object AudioRecorder {
-
     private var isRecording = false
 
     private var sampleRateInHz: Int = 44100
@@ -21,16 +20,6 @@ object AudioRecorder {
 
     private lateinit var audioRecord: AudioRecord
     private lateinit var audioTrack: AudioTrack
-
-    private fun doRecord() {
-        isRecording = true
-        val buffer = ByteArray(bufferSize)
-        while (isRecording) {
-            val byteSize = audioRecord.read(buffer, 0, bufferSize)
-            if (byteSize >= AudioRecord.SUCCESS)
-                audioTrack.write(buffer, 0, byteSize)
-        }
-    }
 
     @SuppressLint("MissingPermission")
     fun doStart() {
@@ -44,7 +33,7 @@ object AudioRecorder {
             bufferSize
         )
         audioTrack = AudioTrack(
-            AudioManager.STREAM_MUSIC,
+            AudioManager.STREAM_SYSTEM,
             sampleRateInHz,
             AudioFormat.CHANNEL_OUT_STEREO,
             AudioFormat.ENCODING_PCM_16BIT,
@@ -57,6 +46,16 @@ object AudioRecorder {
 
         thread {
             doRecord()
+        }
+    }
+
+    private fun doRecord() {
+        isRecording = true
+        val buffer = ByteArray(bufferSize)
+        while (isRecording) {
+            val byteSize = audioRecord.read(buffer, 0, bufferSize)
+            if (byteSize >= AudioRecord.SUCCESS)
+                audioTrack.write(buffer, 0, byteSize)
         }
     }
 
