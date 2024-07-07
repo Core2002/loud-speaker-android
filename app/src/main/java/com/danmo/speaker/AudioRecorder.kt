1dbf9b4200d99ea0735b7c4426304c01584b8e68
Ruv2005 <2099246846@qq.com>
2024-07-07 22:39:03 +0800

    Fetch: MODIFY PACKAGE NAME.

---
+++ app/src/main/java/com/danmo/speaker/AudioRecorder.kt
@@ -0,0 +1,72 @@
+package com.my
+
+import android.annotation.SuppressLint
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioRecord
+import android.media.AudioTrack
+import android.media.MediaRecorder
+import kotlin.concurrent.thread
+
+object AudioRecorder {
+    private var isRecording = false
+
+    private var sampleRateInHz: Int = 44100
+    private var bufferSize = AudioRecord.getMinBufferSize(
+        sampleRateInHz,
+        AudioFormat.CHANNEL_IN_STEREO,
+        AudioFormat.ENCODING_PCM_16BIT
+    )
+
+    private lateinit var audioRecord: AudioRecord
+    private lateinit var audioTrack: AudioTrack
+
+    @SuppressLint("MissingPermission")
+    fun doStart() {
+        if (isRecording) return
+
+        audioRecord = AudioRecord(
+            MediaRecorder.AudioSource.MIC,
+            sampleRateInHz,
+            AudioFormat.CHANNEL_IN_STEREO,
+            AudioFormat.ENCODING_PCM_16BIT,
+            bufferSize
+        )
+        audioTrack = AudioTrack(
+            AudioManager.STREAM_SYSTEM,
+            sampleRateInHz,
+            AudioFormat.CHANNEL_OUT_STEREO,
+            AudioFormat.ENCODING_PCM_16BIT,
+            bufferSize,
+            AudioTrack.MODE_STREAM
+        )
+
+        audioRecord.startRecording()
+        audioTrack.play()
+
+        thread {
+            doRecord()
+        }
+    }
+
+    private fun doRecord() {
+        isRecording = true
+        val buffer = ByteArray(bufferSize)
+        while (isRecording) {
+            val byteSize = audioRecord.read(buffer, 0, bufferSize)
+            if (byteSize >= AudioRecord.SUCCESS)
+                audioTrack.write(buffer, 0, byteSize)
+        }
+    }
+
+    fun doStop() {
+        if (!isRecording) return
+
+        isRecording = false
+        audioRecord.stop()
+        audioTrack.stop()
+        audioRecord.release()
+        audioTrack.release()
+    }
+
+}
