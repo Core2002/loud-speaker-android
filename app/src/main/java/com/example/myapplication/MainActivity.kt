60d0a772d02f31d6ab9759f5d0af76479c4e1682
Ruv2005 <2099246846@qq.com>
2024-07-07 21:52:56 +0800

    Fetch: ADD STATUSTEXT.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,6 +38,9 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun RecordMainPage() {
+    var statusText by remember {
+        mutableStateOf("欢迎使用")
+    }
     val modifier = Modifier
         .fillMaxSize()
         .wrapContentSize(Alignment.Center)
@@ -43,15 +50,20 @@ fun RecordMainPage() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(32.dp)
     ) {
+        Text(
+            text = statusText,
+            fontSize = 28.sp
+        )
         Button(
             modifier = Modifier
                 .width(200.dp)
                 .height(80.dp),
             onClick = {
+                statusText = "捕获已启用"
                 AudioRecorder.doStart()
             }) {
             Text(
-                text = "Start",
+                text = "开始",
                 fontSize = 32.sp
             )
         }
@@ -61,10 +73,11 @@ fun RecordMainPage() {
                 .width(200.dp)
                 .height(80.dp),
             onClick = {
+                statusText = "捕获已结束"
                 AudioRecorder.doStop()
             }) {
             Text(
-                text = "Stop",
+                text = "停止",
                 fontSize = 32.sp
             )
         }
