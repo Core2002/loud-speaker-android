162df69d06221fabb01d60d7e2083caf6e25d183
钟小白Core <42798150+core2002@users.noreply.github.com>
2024-07-09 20:35:34 +0800

    Fetch: Create LICENSE.

--- app/src/main/java/com/example/myapplication/FormatTime.kt
+++ app/src/main/java/com/example/myapplication/FormatTime.kt
@@ -4,9 +4,9 @@ import java.util.concurrent.TimeUnit.MILLISECONDS
 
 object FormatTime {
     fun formatTime(millis: Long): String {
-        val seconds = MILLISECONDS.toSeconds(millis)
-        val minutes = MILLISECONDS.toMinutes(millis)
-        val hours = MILLISECONDS.toHours(millis)
+        val seconds = MILLISECONDS.toSeconds(millis) % 24
+        val minutes = MILLISECONDS.toMinutes(millis) % 60
+        val hours = MILLISECONDS.toHours(millis) % 60
         return "$hours 小时 $minutes 分钟 $seconds 秒"
     }
 }
